d21e33a679bc22b4936d37b93d8298fbc577f2e5
Simone Del Gais <86740528+SimoneDG29@users.noreply.github.com>
2026-05-06 12:01:21 +0200

    feat: add products-service microservice with PostgreSQL support
    * feat: scaffold products-service
    
    * feat: dockerize products-service
    
    * feat: add products-service and postgres to compose
    
    * feat: init products database schema
    
    * feat: add products api endpoints
    
    * test: add products api unit tests
    
    * ci: add products-service tests to lint-test
    
    * fix: resolve module import paths in products-service tests
    
    * ci: add products-service image to release pipeline
    
    * feat: add products-service and postgres to helm and env values injection with Github Secrets

---
+++ services/products-service/app.py
@@ -0,0 +1,11 @@
+from db import init_db
+from flask import Flask
+from routes import register_routes
+
+app = Flask(__name__)
+
+register_routes(app)
+
+if __name__ == "__main__":
+    init_db()
+    app.run(host="0.0.0.0", port=5003)
